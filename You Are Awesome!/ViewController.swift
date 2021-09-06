5eb3758075fc511143091999c4e38c7e1939bd48
Richard Shum <richard18shum@gmail.com>
2021-09-06 14:49:47 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Richard Shum on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messageLabel.text = ""
+    }
+    
+    @IBAction func showMessagePressed(_ sender: UIButton)  {
+        print(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+    }
+    
+}
+
